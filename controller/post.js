7ea4e0c24c5f535f58476ff4602d930c81193fb3
Ayush Mishra <46623370+AyushWebDev@users.noreply.github.com>
2020-05-18 20:03:28 +0530

    Add files via upload

--- controller/post.js
+++ controller/post.js
@@ -6,6 +6,8 @@ const _=require("lodash");
 exports.postById=(req,res,next,id)=>{
     Post.findById(id) 
     .populate("postedBy","_id name")
+    // .populate("comments","text created")
+     .populate("comments.postedBy","_id name")
     .exec((err,post)=>{
         if(err){
             return res.status(400).json({
@@ -31,6 +33,8 @@ exports.getPosts=(req,res)=>{
     
     const posts=Post.find()
     .populate("postedBy","_id name")//as it is refering to different model
+    .populate("comments","text created")
+    .populate("comments.postedBy","_id name")
     .select("_id title body created likes")//for properties of same model
     .sort({created: -1})
     .then((posts)=>{
@@ -186,7 +190,7 @@ exports.like=(req,res,next)=>{
         return res.status(400).json({
             error: err
         })
-        else
+        
         return res.json(result);
     })
 }
@@ -200,5 +204,38 @@ exports.unlike=(req,res)=>{
         })
         else
             return res.json(result);
+    })
+}
+
+exports.comment=(req,res)=>{
+    let comment=req.body.comment;
+    comment.postedBy=req.body.userId;
+    Post.findByIdAndUpdate(req.body.postId,{$push: {comments: comment}},{new: true})
+    // .populate("comments","text created")
+    .populate("comments.postedBy","_id name")
+    // .populate("postedBy","_id name")
+    .exec((err,result)=>{
+        if(err)
+            return res.status(400).json({
+                error: err
+            })
+        else
+            return res.json(result)
+    })
+}
+
+exports.uncomment=(req,res)=>{
+    let comment=req.body.comment;
+    
+    Post.findByIdAndUpdate(req.body.postId,{$pull: {comments: {_id: comment._id}}},{new: true})
+    .populate("comments.postedBy","_id name")
+    //.populate("postedBy","_id name")
+    .exec((err,result)=>{
+        if(err)
+            return res.status(400).json({
+                error: err
+            })
+        else
+            return res.json(result)
     })
 }
